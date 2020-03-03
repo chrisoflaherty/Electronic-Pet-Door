62a319b6fbc963610803b8f7261a5c8c0d0064bb
chrisoflaherty <chrisoflaherty@me.com>
2020-03-03 14:06:58 -0500

    Update configuration_mode.py

--- configuration_mode.py
+++ configuration_mode.py
@@ -183,17 +183,17 @@ def get_schedule():
             q = f.readline()
             if q == '0':
                 sched["1200"+ampm] = 0
-                pop["1200"+ampm] = 0
+                pop["1200"+ampm] = ""
             else:
                 sched["1200"+ampm] = 1
-                pop["1200"+ampm] = 1
+                pop["1200"+ampm] = "checked"
             q = f.readline()
             if q == '0':
                 sched["1230"+ampm] = 0
-                pop["1230"+ampm] = 0
+                pop["1230"+ampm] = ""
             else:
-                sched["1230"+ampm] = 0
-                pop["1230"+ampm] = 0
+                sched["1230"+ampm] = 1
+                pop["1230"+ampm] = "checked"
             
             for i in range(1,12,1):
                 if i < 10:
@@ -374,7 +374,7 @@ def web_server():
 
     while True:
         with open("credentials.txt", "r") as creds:
-            if creads.read == "":
+            if creds.read == "":
                 print("Reset button clicked.\n")
                 break
         conn, addr = s.accept()
